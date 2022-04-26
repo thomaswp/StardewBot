313e1a8711401322bf361bd5048ca2564eb97b4b
Thomas Price <thomaswprice1@gmail.com>
2022-04-26 08:24:36 -0400

    First commit

---
+++ StardewBot/ModEntry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewBot
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        private IModHelper helper;
+        private NPC robot;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            this.helper = helper;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+        }
+
+        private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
+        {
+            Farm farm = Game1.getFarm();
+            NPC robin = Game1.getCharacterFromName("Robin");
+            robin.setTilePosition(farm.GetMainFarmHouseEntry() + new Point(0, 5));
+            farm.addCharacter(robin);
+
+            robot = robin;
+
+            // print button presses to the console window
+            Monitor.Log($"Spawned robot: {robot.position}", LogLevel.Debug);
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            if (robot != null && e.Button == SButton.O)
+            {
+                //robot.tryToMoveInDirection(0, false, 0, false);
+                robot.jump();
+            }
+
+        }
+    }
+}
